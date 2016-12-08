baf0c3956a05e3a8acbf7eccc40cd5b000cba664
ryWong <1205746763@qq.com>
2016-12-08 14:42:27 +0800

    新建工程
    這是一個新的工程

---
+++ TestingWePlay/TestingWePlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestingWePlay
+//
+//  Created by RAYMOND on 2016/12/8.
+//  Copyright © 2016年 TR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestingWePlay/TestingWePlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestingWePlay
+//
+//  Created by RAYMOND on 2016/12/8.
+//  Copyright © 2016年 TR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
